48c38a8977ec4fec8883cffdcaa3a8c99d089be1
Ken Williams <kwilliams@cpan.org>
2002-11-28 01:45:35 +0000

    Some fixes for MacOS from Schwern:
     - set 'sitelib' and 'sitearch' from 'installsitelib' and 'installsitearch'
    
     - Set creator/type of the 'Build' script to 'McPL', 'TEXT' so we can
       double-click on it.
    
     - chmod(666) in rm_previous_build_script() and ACTION_realclean() to
       unlock before removing (may not be necessary anymore, with
       File::Path doing the removal?)
    
     - in dispatch(), if there are no args (@_ or @ARGV), pop up a dialog
       box to ask the user what to do.
    
    
    git-svn-id: http://svn.perl.org/modules/Module-Build/trunk@3908 50811bd7-b8ce-0310-adc1-d9db26280581

--- lib/Module/Build/Platform/MacOS.pm
+++ lib/Module/Build/Platform/MacOS.pm
@@ -7,9 +7,72 @@ use vars qw(@ISA);
 @ISA = qw(Module::Build::Base);
 
 
+sub new {
+  my $class = shift;
+  my $self = $class->SUPER::new(@_);
+  
+  $self->{config}{sitelib}  ||= $self->{config}{installsitelib};
+  $self->{config}{sitearch} ||= $self->{config}{installsitearch};
+
+  return $self;
+}
+
+sub make_build_script_executable {
+  my $self = shift;
+	
+  # Can't hurt to make it read-only.
+  $self->SUPER::make_build_script_executable;
+	
+  require MacPerl;
+  MacPerl::SetFileInfo('McPL', 'TEXT', $self->{properties}{build_script});
+}
+
+sub rm_previous_build_script {
+  my $self = shift;
+    
+  if( $self->{properties}{build_script} ) {
+    chmod 666, $self->{properties}{build_script};
+  }
+  $self->SUPER::rm_previous_build_script;
+}
+
+sub dispatch {
+  my $self = shift;
+
+  if( !@_ and !@ARGV ) {
+    require MacPerl;
+      
+    # What comes first in the action list.
+    my @action_list = qw(test install build);
+    my %actions;
+    {
+      no strict 'refs';
+    
+      foreach my $class ($self->super_classes) {
+        foreach ( keys %{ $class . '::' } ) {
+          $actions{$1}++ if /ACTION_(\w+)/;
+        }
+      }
+    }
+  
+    delete @actions{@action_list};
+    push @action_list, sort { $a cmp $b } keys %actions;
+    $ARGV[0] = MacPerl::Pick('What build command?', @action_list);
+    push @ARGV, split /\s+/, 
+                  MacPerl::Ask('Any extra arguments?  (ie. verbose=1)', '');
+  }
+  
+  $self->SUPER::dispatch(@_);
+}
+
+sub ACTION_realclean {
+  my $self = shift;
+  chmod 666, $self->{properties}{build_script};
+  $self->SUPER::ACTION_realclean;
+}
+
 1;
 __END__
-
 
 =head1 NAME
 
@@ -18,11 +81,46 @@ Module::Build::Platform::MacOS - Builder class for MacOS platforms
 =head1 DESCRIPTION
 
 The sole purpose of this module is to inherit from
-C<Module::Build::Base>.  Please see the L<Module::Build> for the docs.
+C<Module::Build::Base> and override a few methods.  Please see
+L<Module::Build> for the docs.
+
+=head2 Overriden Methods
+
+=over 4
+
+=item new()
+
+MacPerl doesn't define $Config{sitelib} or $Config{sitearch} for some
+reason, but $Config{installsitelib} and $Config{installsitearch} are
+there.  So we copy the install variables to the other location
+
+=item make_build_script_executable()
+
+On MacOS we set the file type and creator to MacPerl so it will run
+with a double-click.
+
+=item rm_previous_build_script()
+
+MacOS maps chmod -w to locking the file.  This mean we have to unlock
+it before removing it.
+
+=item dispatch()
+
+Because there's no easy way to say "./Build test" on MacOS, if
+dispatch is called with no arguments and no @ARGV a dialog box will
+pop up asking what action to take and any extra arguments.
+
+Default action is "test".
+
+=item ACTION_realclean()
+
+Need to unlock the Build program before deleting.
+
+=back
 
 =head1 AUTHOR
 
-Ken Williams, ken@forum.swarthmore.edu
+Michael G Schwern <schwern@pobox.com>
 
 =head1 SEE ALSO
 
