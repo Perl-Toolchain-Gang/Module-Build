48b510ae1ee5cd5f0e7aa9afe990fa91925e71cf
Ken Williams <kwilliams@cpan.org>
2002-12-13 02:58:16 +0000

    Move some common functionality into its own script
    git-svn-id: http://svn.perl.org/modules/Module-Build/trunk@3935 50811bd7-b8ce-0310-adc1-d9db26280581

---
+++ t/common.pl
@@ -0,0 +1,23 @@
+
+sub stdout_of {
+  my $subr = shift;
+  my $outfile = 'save_out';
+
+  local *SAVEOUT;
+  open SAVEOUT, ">&STDOUT" or die "Can't save STDOUT handle: $!";
+  open STDOUT, "> $outfile" or die "Can't create $outfile: $!";
+
+  eval {$subr->()};
+  open STDOUT, ">&SAVEOUT" or die "Can't restore STDOUT: $!";
+#  die $@ if $@;
+
+  return slurp($outfile);
+}
+
+sub slurp {
+  open my($fh), $_[0] or die "Can't open $_[0]: $!";
+  local $/;
+  return <$fh>;
+}
+
+1;
