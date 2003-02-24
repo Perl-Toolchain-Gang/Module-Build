cdb277c11c127b09e77d3c14094d6e3538e7c00a
Ken Williams <kwilliams@cpan.org>
2003-02-24 18:25:07 +0000

    Version bump to 0.16
    git-svn-id: http://svn.perl.org/modules/Module-Build/trunk@4004 50811bd7-b8ce-0310-adc1-d9db26280581

--- lib/Module/Build.pm
+++ lib/Module/Build.pm
@@ -12,7 +12,7 @@ use File::Path ();
 use File::Basename ();
 
 use vars qw($VERSION @ISA);
-$VERSION = '0.15';
+$VERSION = '0.16';
 
 # Okay, this is the brute-force method of finding out what kind of
 # platform we're on.  I don't know of a systematic way.  These values
