98debd0440e35042422d021d9f4ac49e67498701
Ken Williams <kwilliams@cpan.org>
2003-03-11 16:29:29 +0000

    Make a .bat file for executables during make_executable()
    git-svn-id: http://svn.perl.org/modules/Module-Build/trunk@4020 50811bd7-b8ce-0310-adc1-d9db26280581

--- lib/Module/Build/Platform/Windows.pm
+++ lib/Module/Build/Platform/Windows.pm
@@ -15,8 +15,9 @@ sub new {
   my $class = shift;
   my $self = $class->SUPER::new(@_);
 
-  # Check compiler type.
   my $cf = $self->{config};
+
+  # Check compiler type.
   if ( $cf->{cc} =~ /cl(\.exe)?$/ ) {
     $cf->{ccname} = 'MSVC';
   } elsif ( $cf->{cc} =~ /bcc32(\.exe)?$/ ) {
@@ -28,6 +29,21 @@ sub new {
   # Inherit from an appropriate compiler driver class
   my $cc_driver = "Module::Build::Platform::Windows::$cf->{ccname}";
   unshift @ISA, $cc_driver;
+
+  # Find 'pl2bat.bat' utility used for installing perl scripts.
+  # This search is probably overkill, as I've never met a MSWin32 perl
+  # where these locations differed from each other.
+  my @potential_dirs = map { File::Spec->canonpath($_) }
+    @${cf}{qw(installscript installbin installsitebin installvendorbin)},
+    File::Basename::dirname($self->{properties}{perl});
+
+  foreach my $dir (@potential_dirs) {
+    my $potential_file = File::Spec->catfile($dir, 'pl2bat.bat');
+    if ( -f $potential_file && !-d _ ) {
+      $cf->{pl2bat} = $potential_file;
+      last;
+    }
+  }
 
   return $self;
 }
@@ -187,6 +203,35 @@ sub normalize_filespecs {
     } else {
       die "Don't know how to normalize " . (ref $spec || $spec) . "\n";
     }
+  }
+}
+
+sub make_executable {
+  my $self = shift;
+  $self->SUPER::make_executable(@_);
+
+  my $pl2bat = $self->{config}{pl2bat};
+
+  if ( defined($pl2bat) && length($pl2bat) ) {
+    foreach my $script (@_) {
+      # Don't run 'pl2bat.bat' for the 'Build' script;
+      # there is no easy way to get the resulting 'Build.bat'
+      # to delete itself when doing a 'Build realclean'.
+      next if ( $script eq $self->{properties}{build_script} );
+
+      (my $script_bat = $script) =~ s/\.plx?//i;
+      $script_bat .= '.bat' unless $script_bat =~ /\.bat$/i;
+
+      my $status = $self->do_system($pl2bat, '<', $script, '>', $script_bat);
+      if ( $status && -f $script_bat ) {
+        $self->SUPER::make_executable($script_bat);
+      } else {
+        warn "Unable to convert '$script' to an executable.\n";
+      }
+    }
+  } else {
+    warn "Could not find 'pl2bat.bat' utility needed to make scripts executable.\n"
+       . "Unable to convert scripts ( " . join(', ', @_) . " ) to executables.\n";
   }
 }
 
