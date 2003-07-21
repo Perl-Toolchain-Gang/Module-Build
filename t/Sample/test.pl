bc3167e08115fd40ebeaaca0c9a5f408930bb1c7
Ken Williams <kwilliams@cpan.org>
2003-07-21 16:38:43 +0000

    A little more helpful test code
    git-svn-id: http://svn.perl.org/modules/Module-Build/trunk@4267 50811bd7-b8ce-0310-adc1-d9db26280581

--- t/Sample/test.pl
+++ t/Sample/test.pl
@@ -5,8 +5,5 @@ plan tests => 2;
 
 ok 1;
 
-# Make sure Module::Build was loaded from blib/
 require Module::Build;
-print "\$INC{'Module/Build.pm'}: $INC{'Module/Build.pm'}\n";
-ok $INC{'Module/Build.pm'}, qr/blib/;
-print "Done.\n";
+ok $INC{'Module/Build.pm'}, qr/blib/, 'Module::Build should be loaded from blib';
