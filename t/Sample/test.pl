3b510302afc282008494b73b595dce775fb9c49c
Ken Williams <kwilliams@cpan.org>
2003-08-16 15:00:25 +0000

    More diagnostic information on failure
    git-svn-id: http://svn.perl.org/modules/Module-Build/trunk@4359 50811bd7-b8ce-0310-adc1-d9db26280581

--- t/Sample/test.pl
+++ t/Sample/test.pl
@@ -7,4 +7,6 @@ ok 1;
 
 require Module::Build;
 ok $INC{'Module/Build.pm'}, qr/blib/, 'Module::Build should be loaded from blib';
+print "# Cwd: ", Module::Build->cwd, "\n";
+print "# \@INC: (@INC)\n";
 print "Done.\n";  # t/compat.t looks for this
