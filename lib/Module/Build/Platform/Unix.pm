ceca69da81106a7193f194ae890a73b54b7c6020
Ken Williams <kwilliams@cpan.org>
2003-10-23 00:34:20 +0000

    Fix up the syntax "FOO=BAR /bin/command arg arg" in $Config{ld} so that it'll run.
    git-svn-id: http://svn.perl.org/modules/Module-Build/trunk@4546 50811bd7-b8ce-0310-adc1-d9db26280581

--- lib/Module/Build/Platform/Unix.pm
+++ lib/Module/Build/Platform/Unix.pm
@@ -6,6 +6,24 @@ use Module::Build::Base;
 use vars qw(@ISA);
 @ISA = qw(Module::Build::Base);
 
+sub link_c {
+  my $self = shift;
+  my $cf = $self->{config};
+  
+  # Some platforms (notably Mac OS X 10.3, but some others too) expect
+  # the syntax "FOO=BAR /bin/command arg arg" to work in %Config
+  # (notably $Config{ld}).  It usually works in system(SCALAR), but we
+  # use system(LIST). We fix it up here with 'env'.
+  
+  local $cf->{ld} = $cf->{ld};
+  if (ref $cf->{ld}) {
+    unshift @{$cf->{ld}}, 'env' if $cf->{ld}[0] =~ /^\s*\w+=/;
+  } else {
+    $cf->{ld} =~ s/^(\s*\w+=)/env $1/;
+  }
+  
+  return $self->SUPER::link_c(@_);
+}
 
 sub make_tarball {
   my ($self, $dir) = @_;
