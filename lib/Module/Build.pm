2f377a7af438123141ef6b10a28fbeefb7cf19e8
Ken Williams <kwilliams@cpan.org>
2004-11-30 13:10:49 +0000

    Version bump for release
    git-svn-id: http://svn.perl.org/modules/Module-Build/branches/release-0_26_branch@5109 50811bd7-b8ce-0310-adc1-d9db26280581

--- lib/Module/Build.pm
+++ lib/Module/Build.pm
@@ -15,7 +15,7 @@ use Module::Build::Base;
 
 use vars qw($VERSION @ISA);
 @ISA = qw(Module::Build::Base);
-$VERSION = '0.2604';
+$VERSION = '0.2605';
 $VERSION = eval $VERSION;
 
 # Okay, this is the brute-force method of finding out what kind of
