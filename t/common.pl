daaae561821ca8a24b4e7f831ee9de4b59ee052a
Randy Sims <randys@thepierianspring.org>
2005-06-29 07:12:53 +0000

    Remove unused subs that provided support for the 'Test' module that is no longer used.
    git-svn-id: http://svn.perl.org/modules/Module-Build/trunk@5481 50811bd7-b8ce-0310-adc1-d9db26280581

--- t/common.pl
+++ t/common.pl
@@ -9,28 +9,12 @@ BEGIN {
   push @INC, $t_lib; # Let user's installed version override
 }
 
-use Test();
 
 sub have_module {
   my $module = shift;
   return eval "use $module; 1";
 }
 
-sub need_module {
-  my $module = shift;
-  skip_test("$module not installed") unless have_module($module);
-}
-
-sub skip_test {
-  my $msg = @_ ? shift() : '';
-  print "1..0 # Skipped: $msg\n";
-  exit;
-}
-
-sub skip_subtest {
-  my $msg = @_ ? shift() : '(no reason given)';
-  Test::skip "skip $msg", 1;
-}
 
 sub save_handle {
   my ($handle, $subr) = @_;
