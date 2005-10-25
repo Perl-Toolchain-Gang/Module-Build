855ba16366a0c9610c4238bb54626b699784eb3a
Randy Sims <randys@thepierianspring.org>
2005-10-25 20:26:01 +0000

    Fix accursed test to check for $node->{$name} rather than $node->{name}; check that it is both defined and non-empty; and make the failure message look like an error.
    git-svn-id: http://svn.perl.org/modules/Module-Build/trunk@5676 50811bd7-b8ce-0310-adc1-d9db26280581

--- lib/Module/Build/Base.pm
+++ lib/Module/Build/Base.pm
@@ -2834,8 +2834,10 @@ sub prepare_metadata {
   foreach (qw(dist_name dist_version dist_author dist_abstract license)) {
     (my $name = $_) =~ s/^dist_//;
     $node->{$name} = $self->$_();
-    die "Missing required field '$name' for META.yml\n" unless length($node->{name});
+    die "ERROR: Missing required field '$name' for META.yml\n"
+      unless defined($node->{$name}) && length($node->{$name});
   }
+
   if (defined( $self->license ) &&
       defined( my $url = $self->valid_licenses->{ $self->license } )) {
     $node->{resources}{license} = $url;
