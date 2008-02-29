bf4a3b12d13d9cdb446ddf0697b8ca8281c43718
Ken Williams <kwilliams@cpan.org>
2008-02-29 18:56:45 +0000

    Ensure we're not Terse when using Data::Dumper
    git-svn-id: http://svn.perl.org/modules/Module-Build/trunk@10854 50811bd7-b8ce-0310-adc1-d9db26280581

--- lib/Module/Build/Dumper.pm
+++ lib/Module/Build/Dumper.pm
@@ -9,7 +9,7 @@ use Data::Dumper;
 sub _data_dump {
   my ($self, $data) = @_;
   return ("do{ my "
-	  . Data::Dumper->new([$data],['x'])->Purity(1)->Dump()
+	  . Data::Dumper->new([$data],['x'])->Purity(1)->Terse(0)->Dump()
 	  . '$x; }')
 }
 
