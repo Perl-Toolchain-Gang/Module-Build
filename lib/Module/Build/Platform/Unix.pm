ec7831f9f9599071ce419c98915fffb205216c2f
Ken Williams <kwilliams@cpan.org>
2008-06-08 03:56:28 +0000

    Allow dashes in Unix usernames
    git-svn-id: http://svn.perl.org/modules/Module-Build/trunk@11391 50811bd7-b8ce-0310-adc1-d9db26280581

--- lib/Module/Build/Platform/Unix.pm
+++ lib/Module/Build/Platform/Unix.pm
@@ -44,7 +44,7 @@ sub _construct {
 
 sub _detildefy {
   my ($self, $value) = @_;
-  $value =~ s[^~(\w*)(?=/|$)]   # tilde with optional username
+  $value =~ s[^~(\w[-\w]*)?(?=/|$)]   # tilde with optional username
     [$1 ?
      ((getpwnam $1)[7] || "~$1") :
      (getpwuid $>)[7]
