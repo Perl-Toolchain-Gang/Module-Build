cd6084154ce293ec5378ef963cd7c437a1f9e656
David Golden <dagolden@cpan.org>
2010-04-01 11:33:33 -0400

    bump version to 0.36_08

--- lib/Module/Build/Base.pm
+++ lib/Module/Build/Base.pm
@@ -4,7 +4,7 @@ package Module::Build::Base;
 
 use strict;
 use vars qw($VERSION);
-$VERSION = '0.3607';
+$VERSION = '0.36_08';
 $VERSION = eval $VERSION;
 BEGIN { require 5.00503 }
 
--- lib/Module/Build/Compat.pm
+++ lib/Module/Build/Compat.pm
@@ -2,7 +2,7 @@ package Module::Build::Compat;
 
 use strict;
 use vars qw($VERSION);
-$VERSION = '0.3607';
+$VERSION = '0.36_08';
 
 use File::Basename ();
 use File::Spec;
