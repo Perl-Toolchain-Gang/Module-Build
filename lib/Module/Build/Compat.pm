aed534590de811fb9cc34fa07166b2c141217db7
David Golden <dagolden@cpan.org>
2011-02-19 20:55:01 -0500

    Changes, lib/***.pm - bump version

--- lib/Module/Build/Compat.pm
+++ lib/Module/Build/Compat.pm
@@ -2,7 +2,7 @@ package Module::Build::Compat;
 
 use strict;
 use vars qw($VERSION);
-$VERSION = '0.37_05';
+$VERSION = '0.37_06';
 
 use File::Basename ();
 use File::Spec;
--- lib/Module/Build/PodParser.pm
+++ lib/Module/Build/PodParser.pm
@@ -2,7 +2,7 @@ package Module::Build::PodParser;
 
 use strict;
 use vars qw($VERSION);
-$VERSION = '0.37_05';
+$VERSION = '0.37_06';
 $VERSION = eval $VERSION;
 use vars qw(@ISA);
 
