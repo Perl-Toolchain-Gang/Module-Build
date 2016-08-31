44330fe4e719804493a92c0dab09b20eeb0257b0
Olivier Mengué <dolmen@cpan.org>
2016-08-31 18:53:18 +0200

    make_tarball: workaround for broken tar on Darwin
    On OS X /usr/bin/tar bundles extended attributes that trigger warnings
    when the tarball is unpacked with GNU tar.
    See http://cpants.cpanauthors.org/kwalitee/no_pax_headers
    
    So when the tarball is made using a tar executable on Darwin we take
    care of setting the appropriate environment variables to avoid the broken
    behaviour. The fix is the same as in ExtUtils::MM_Darwin.

--- lib/Module/Build/Base.pm
+++ lib/Module/Build/Base.pm
@@ -4946,6 +4946,14 @@ sub make_tarball {
 
   if ($self->{args}{tar}) {
     my $tar_flags = $self->verbose ? 'cvf' : 'cf';
+
+    # See ExtUtils::MM_Darwin
+    # 10.4 wants COPY_EXTENDED_ATTRIBUTES_DISABLE.
+    # 10.5 wants COPYFILE_DISABLE.
+    # So just set both.
+    local $ENV{COPY_EXTENDED_ATTRIBUTES_DISABLE} = 1 if $^O eq 'darwin';
+    local $ENV{COPYFILE_DISABLE}                 = 1 if $^O eq 'darwin';
+
     $self->do_system($self->split_like_shell($self->{args}{tar}), $tar_flags, "$file.tar", $dir);
     $self->do_system($self->split_like_shell($self->{args}{gzip}), "$file.tar") if $self->{args}{gzip};
   } else {
